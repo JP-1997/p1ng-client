6363b2a680e452e36fd66d808e8bd07cedb79048
JP-1997 <jyotiprakash.jpl.99@gmail.com>
2021-02-26 23:24:00 +0530

    add ChatRoomScreen component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import ChatsListScreen from "./components/ChatsListScreen";
-import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+  RouteComponentProps,
+} from "react-router-dom";
 import ChatRoomScreen from "./components/ChatRoomScreen";
 
 const App: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/chats" component={ChatsListScreen} />
-      <Route exact path="/chats/:chatId" component={ChatRoomScreen} />
+      <Route
+        exact
+        path="/chats/:chatId"
+        component={({ match }: RouteComponentProps<{ chatId: string }>) => (
+          <ChatRoomScreen chatId={match.params.chatId} />
+        )}
+      />
     </Switch>
     <Route exact path="/" render={redirectToChats} />
   </BrowserRouter>
